f3f89f6d9b73b85a931c051674cda89b5c132bab
WenyiLULU <luwenyi1991@gmail.com>
2022-05-10 14:09:52 +0200

    1-3 done

--- js/index.js
+++ js/index.js
@@ -2,6 +2,15 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  
+  const price = parseFloat(product.querySelector('.price span').innerHTML);
+  const quantity = parseInt(product.querySelector('.quantity input').value);
+  const subtotal = price * quantity;
+
+  const subtotalElement = product.querySelector('.subtotal span');
+  subtotalElement.innerHTML = subtotal;
+
+  return subtotal
 
   //... your code goes here
 }
@@ -9,13 +18,19 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
-
+  const allProduct = document.querySelectorAll('.product')
+  let sum = 0;
+  for (let i = 0; i < allProduct.length; i+=1){
+    sum += updateSubtotal(allProduct[i]);
+  }
+  const totalAll = document.getElementById('total-value').querySelector('span')
+  totalAll.innerHTML = sum;
+  
   // ITERATION 3
   //... your code goes here
 }
